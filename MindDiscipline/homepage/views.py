29ef4666c4ca86a0ac777952e7546bdf953815b7
Nenamanui <zakharov-kirl@yandex.ru>
2024-10-31 01:22:04 +0300

    add homepage.html

--- MindDiscipline/homepage/views.py
+++ MindDiscipline/homepage/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):    
-    return HttpResponse('Главная страница') 
+def index(request):
+    template_name = 'homepage/homepage.html'
+    return render(request, template_name)
 
 def practice(request, pk):
     return HttpResponse('Тренировка номер', pk)
--- MindDiscipline/practice/views.py
+++ MindDiscipline/practice/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):    
-    return HttpResponse('practica') 
+def practices(request):    
+    return HttpResponse('practices')
+
+def practice(request, pk)
+    return HttpResponse('Тренировка номер' pk)
