78455c8328b64f68f32f60de60839616209ca41a
Nenamanui <zakharov-kirl@yandex.ru>
2024-11-12 00:52:03 +0300

    added login page

--- MindDiscipline/MindDiscipline/urls.py
+++ MindDiscipline/MindDiscipline/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
+    path('account/', include('django.contrib.auth.urls')),
     path('coach/', include('coach.urls')),
     path('child/', include('child.urls')),
     path('practice/', include('practice.urls')),
