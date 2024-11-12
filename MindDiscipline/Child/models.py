fccb07f0b62af4afd85e89aab0f5fa44efa7deb7
Nenamanui <zakharov-kirl@yandex.ru>
2024-11-12 16:02:02 +0300

    first migrations and init child model

--- MindDiscipline/Child/models.py
+++ MindDiscipline/Child/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Child(models.Model):
+    nickname = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=50)
+    main_sport = models.CharField(max_length=50)
+    extra_sport = models.CharField(max_length=50)
+    birthday = models.DateField()
+
+class Statistics(models.Model):
+    pass 
+    # attention = models.IntegerField(
+    #    validators=[
+    #        MinValueValidator(1),
+    #       MaxValueValidator(10)
+    #   ]
+    #)
+    # удачливость?
+    # уверенность
+    # настроение
+    # ...
+    # 
--- MindDiscipline/Coach/models.py
+++ MindDiscipline/Coach/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Coach(models.Model):
+    nickname = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=50)
+    main_sport = models.CharField(max_length=50)
+    extra_sport = models.CharField(max_length=50)
+    birthday = models.DateField()
+    experience_date = models.PositiveSmallIntegerField()
+    practice = models.ManyToManyField('practice.practice')
