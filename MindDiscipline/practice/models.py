b18726d5a296f359d0ebd299080b7f1ecf407d1a
Nenamanui <zakharov-kirl@yandex.ru>
2024-11-12 15:17:45 +0300

    init model practice

--- MindDiscipline/practice/models.py
+++ MindDiscipline/practice/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Practice(models.Model):
+    type = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    duration = models.DurationField()
+    main_theme = models.CharField(max_length=50)
+    description = models.TextField()
+    # coach = models.ForeignKey(Coach, on_delete=)
+    # child = models.ManyToManyField(Child)
