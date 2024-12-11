6ddbb87a58473fc4d7a68f48db7c09f9d5a26c6c
Nenamanui <zakharov-kirl@yandex.ru>
2024-12-11 17:49:32 +0300

    commit b4 adding actions

--- MindDiscipline/Coach/admin.py
+++ MindDiscipline/Coach/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Coach
 
-admin.site.register(Coach)
+# @admin.action()
+# def 
+
+
+
+class CoachCustomAdmin(admin.ModelAdmin):
+    search_fields = ('last_name', 'first_name', 'patronymic_name', 'main_sport')
+    list_display = ('last_name', 'first_name', 'patronymic_name', 'main_sport')
+    list_display_links = ('last_name', 'first_name', 'patronymic_name')
+    list_filter = ('main_sport', 'extra_sport', 'experience_years')
+    filter_horizontal = ('practice',)
+
+
+admin.site.register(Coach, CoachCustomAdmin)
--- MindDiscipline/practice/models.py
+++ MindDiscipline/practice/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 class Practice(models.Model):
     type = models.CharField(max_length=50)
-    date = models.DateTimeField()
+    date = models.DateTimeField('Дата')
     duration = models.DurationField()
     main_theme = models.CharField(max_length=50)
     description = models.TextField()
-    # coach = models.ForeignKey(Coach, on_delete=)
     child = models.ManyToManyField('child.Child')
